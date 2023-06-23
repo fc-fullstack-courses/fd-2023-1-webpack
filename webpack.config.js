55916e1c5b541725e8a4e3a8b19a86db6aa441cc
Kyrylo <fresh.js.ftd.@gmail.com>
2023-06-23 14:53:17 +0300

    Add basic webpack config

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+const path = require('path');
+
+// console.log(__dirname);
+// console.log(__filename);
+// console.log( path.resolve(__dirname, 'dist'));
+// console.log( path.resolve('/','test', 'string', 'webpack'));
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'my_file.js',
+    path: path.resolve(__dirname, 'build'),
+  },
+};
